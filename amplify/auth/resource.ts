3b30fb95832d833d441015aae1da2f432e493511
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-04-09 13:48:01 +0530

    test

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,7 +9,7 @@ export const auth = defineAuth({
     email: true,
     externalProviders: {
       saml: {
-        name: 'Azure',
+        name: 'Azure2',
         metadata: {
           metadataContent: 'https://login.microsoftonline.com/6c62ce1b-36c4-4f2d-a827-069c37e92080/federationmetadata/2007-06/federationmetadata.xml?appid=6f5f938a-ef6f-4410-98ff-55182070d2fc', // or content of the metadata file
           metadataType: 'URL',
