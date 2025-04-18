541953f7e5629bf29a95a3401fb9c9da2940eb43
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-04-19 00:30:12 +0530

    test4

---
+++ amplify-config.ts
@@ -0,0 +1,13 @@
+export const BUCKET_NAME = "baff-demo-storage-browser";
+export const BUCKET_REGION = "us-east-1";
+
+export const SAML_METADATA_URL =
+  "https://login.microsoftonline.com/6c62ce1b-36c4-4f2d-a827-069c37e92080/federationmetadata/2007-06/federationmetadata.xml?appid=44302f3d-8474-4981-988d-0bc17cc1d193";
+
+export const CALLBACK_URLS = [
+  "https://default-cognito.d14p5z06ominpn.amplifyapp.com"
+];
+
+export const LOGOUT_URLS = [
+  "https://default-cognito.d14p5z06ominpn.amplifyapp.com"
+];
