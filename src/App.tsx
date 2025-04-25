303ea8ba7abf0c89aa808e813d59458d7cd1ba49
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-04-25 23:49:04 +0530

    adding logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getCurrentUser } from "aws-amplify/auth";
+import { getCurrentUser, signOut } from "aws-amplify/auth";
 import { StorageBrowser } from "../components/Storage";
 import LandingPage from "./LandingPage";
 
@@ -8,14 +8,14 @@ function App() {
   const [userName, setUserName] = useState<string>("");
 
   useEffect(() => {
-    if (window.location.pathname === "/logout-complete") {
-      const tenantId = "6c62ce1b-36c4-4f2d-a827-069c37e92080";
-      const finalRedirectUri = "https://default-cognito.d14p5z06ominpn.amplifyapp.com";
+    // if (window.location.pathname === "/logout-complete") {
+    //   const tenantId = "6c62ce1b-36c4-4f2d-a827-069c37e92080";
+    //   const finalRedirectUri = "https://default-cognito.d14p5z06ominpn.amplifyapp.com";
 
-      const microsoftLogoutUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/logout?post_logout_redirect_uri=${encodeURIComponent(finalRedirectUri)}`;
-      window.location.href = microsoftLogoutUrl;
-      return;
-    }
+    //   const microsoftLogoutUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/logout?post_logout_redirect_uri=${encodeURIComponent(finalRedirectUri)}`;
+    //   window.location.href = microsoftLogoutUrl;
+    //   return;
+    // }
 
     // Normal session check
     getCurrentUser()
@@ -29,12 +29,12 @@ function App() {
       });
   }, []);
 
-  const handleSignOut = () => {
-    const redirectAfterCognito = "https://default-cognito.d14p5z06ominpn.amplifyapp.com/logout-complete";
-    const cognitoLogoutUrl = `https://64d24683309ce5cfce78.auth.us-east-1.amazoncognito.com/logout?client_id=1j2u7spdithuthome35lh84pg1&redirect_uri=${encodeURIComponent(redirectAfterCognito)}`;
+  // const handleSignOut = () => {
+  //   const redirectAfterCognito = "https://default-cognito.d14p5z06ominpn.amplifyapp.com/logout-complete";
+  //   const cognitoLogoutUrl = `https://64d24683309ce5cfce78.auth.us-east-1.amazoncognito.com/logout?client_id=1j2u7spdithuthome35lh84pg1&redirect_uri=${encodeURIComponent(redirectAfterCognito)}`;
 
-    window.location.href = cognitoLogoutUrl;
-  };
+  //   window.location.href = cognitoLogoutUrl;
+  // };
 
   if (isAuthenticated === null) {
     return <p>Loading...</p>;
@@ -45,7 +45,7 @@ function App() {
       <h1>Welcome, {userName}</h1>
       <h2>Storage Browser</h2>
       <StorageBrowser />
-      <button onClick={handleSignOut}>
+      <button onClick={() => signOut()}>
         Sign out
       </button>
     </main>
