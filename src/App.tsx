adb6e2d438b5e93a80b1c5c3d02ebab51b905127
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-04-26 00:00:01 +0530

    adding logout

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,6 @@ function App() {
   const [userName, setUserName] = useState<string>("");
 
   useEffect(() => {
-    // if (window.location.pathname === "/logout-complete") {
-    //   const tenantId = "6c62ce1b-36c4-4f2d-a827-069c37e92080";
-    //   const finalRedirectUri = "https://default-cognito.d14p5z06ominpn.amplifyapp.com";
-
-    //   const microsoftLogoutUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/logout?post_logout_redirect_uri=${encodeURIComponent(finalRedirectUri)}`;
-    //   window.location.href = microsoftLogoutUrl;
-    //   return;
-    // }
-
-    // Normal session check
     getCurrentUser()
       .then((user) => {
         setIsAuthenticated(true);
@@ -28,13 +18,6 @@ function App() {
         setIsAuthenticated(false);
       });
   }, []);
-
-  // const handleSignOut = () => {
-  //   const redirectAfterCognito = "https://default-cognito.d14p5z06ominpn.amplifyapp.com/logout-complete";
-  //   const cognitoLogoutUrl = `https://64d24683309ce5cfce78.auth.us-east-1.amazoncognito.com/logout?client_id=1j2u7spdithuthome35lh84pg1&redirect_uri=${encodeURIComponent(redirectAfterCognito)}`;
-
-  //   window.location.href = cognitoLogoutUrl;
-  // };
 
   if (isAuthenticated === null) {
     return <p>Loading...</p>;
