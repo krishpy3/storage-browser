f82b001d90668f742ae64bd0476fb7eb23434fee
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-04-25 22:47:45 +0530

    adding logout

--- src/App.tsx
+++ src/App.tsx
@@ -19,15 +19,14 @@ function App() {
       });
   }, []);
 
-  const handleSignOut = async () => {
+  const handleSignOut = () => {
     const tenantId = "6c62ce1b-36c4-4f2d-a827-069c37e92080";
     const finalRedirectUri = "https://default-cognito.d14p5z06ominpn.amplifyapp.com";
 
     const microsoftLogoutUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/logout?post_logout_redirect_uri=${encodeURIComponent(finalRedirectUri)}`;
 
-    const cognitoLogoutUrl = `https://64d24683309ce5cfce78.auth.us-east-1.amazoncognito.com/logout?client_id=1j2u7spdithuthome35lh84pg1&logout_uri=${encodeURIComponent(microsoftLogoutUrl)}`;
+    const cognitoLogoutUrl = `https://64d24683309ce5cfce78.auth.us-east-1.amazoncognito.com/logout?client_id=1j2u7spdithuthome35lh84pg1&redirect_uri=${encodeURIComponent(microsoftLogoutUrl)}`;
 
-    // Let Cognito handle the redirect to Microsoft logout
     window.location.href = cognitoLogoutUrl;
   };
 
