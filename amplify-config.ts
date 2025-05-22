e402173e90f63e4bc8dbd4a28bc0d1d16b5edb8e
Krish <krishpy3@gmail.com>
2025-05-23 01:02:41 +0530

    adding Amplify URL

--- amplify-config.ts
+++ amplify-config.ts
@@ -5,5 +5,5 @@ export const TENANT_ID = '6c62ce1b-36c4-4f2d-a827-069c37e92080';
 export const SAML_METADATA_URL =
   "https://login.microsoftonline.com/6c62ce1b-36c4-4f2d-a827-069c37e92080/federationmetadata/2007-06/federationmetadata.xml?appid=44302f3d-8474-4981-988d-0bc17cc1d193";
 
-export const AMPLIFY_URL = "https://default-cognito.d14p5z06ominpn.amplifyapp.com";
+export const AMPLIFY_URL = "https://default-cognito.d2xrjlfxi6eelg.amplifyapp.com";
 
