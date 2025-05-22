a556f025dc3580420f5e96cf457b26d6c3946dd3
Krish <98233937+krishpy3@users.noreply.github.com>
2025-05-23 00:37:27 +0530

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,13 +3,9 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'baff',
   access: (allow) => ({
-    'profile-pictures/{entity_id}/*': [
-      allow.guest.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'picture-submissions/*': [
-      allow.authenticated.to(['read','write']),
-      allow.guest.to(['read', 'write'])
+    'public/*': [
+      allow.groups(['auditor']).to(['read']),
+      allow.groups(['admin']).to(['read', 'write', 'delete'])
     ],
   })
 });
