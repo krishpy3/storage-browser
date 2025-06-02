677ad4432a1de78b0122c3e077c502cc2f50d947
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-06-03 03:24:48 +0530

    change url

--- amplify-config.ts
+++ amplify-config.ts
@@ -1,9 +1,9 @@
 export const BUCKET_NAME = "baff-demo-storage-browser";
 export const BUCKET_REGION = "us-east-1";
-export const TENANT_ID = '6c62ce1b-36c4-4f2d-a827-069c37e92080';
+export const TENANT_ID = "6c62ce1b-36c4-4f2d-a827-069c37e92080";
 
 export const SAML_METADATA_URL =
   "https://login.microsoftonline.com/6c62ce1b-36c4-4f2d-a827-069c37e92080/federationmetadata/2007-06/federationmetadata.xml?appid=44302f3d-8474-4981-988d-0bc17cc1d193";
 
-export const AMPLIFY_URL = "https://default-cognito.d2xrjlfxi6eelg.amplifyapp.com";
-
+export const AMPLIFY_URL =
+  "https://default-cognito.d12yp55vp6fc22.amplifyapp.com";
