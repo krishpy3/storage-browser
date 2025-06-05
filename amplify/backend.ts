eedeb53f57dbf8ba58cb9c6f02f3e6aca5a0d40b
Krish <98233937+krishpy3@users.noreply.github.com>
2025-06-06 00:30:20 +0530

    Add CORS rule to storage bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,24 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { Bucket, HttpMethods } from 'aws-cdk-lib/aws-s3';
 
 defineBackend({
   auth,
   data,
   storage,
 });
+
+// Add CORS configuration to the storage bucket
+const customBucket = storage.resources.bucket as Bucket;
+customBucket.addCorsRule({
+  allowedMethods: [
+    HttpMethods.GET,
+    HttpMethods.PUT,
+    HttpMethods.POST,
+    HttpMethods.DELETE,
+  ],
+  allowedOrigins: ['*'],
+  allowedHeaders: ['*'],
+});
+
