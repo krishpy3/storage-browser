fc072d5f6a60e5ceb1a5b7dbdc03d6ede42a5bb0
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-06-14 00:19:19 +0530

    group test

--- amplify-config.ts
+++ amplify-config.ts
@@ -4,11 +4,10 @@ export const TENANT_ID = "6c62ce1b-36c4-4f2d-a827-069c37e92080";
 
 export const ATTRIBUTE_MAPPING = {
   email: "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
-  // "custom:azure_group":
-  //   "http://schemas.microsoft.com/ws/2008/06/identity/claims/groups",
+  "custom:azure_group": "http://schemas.xmlsoap.org/claims/Group",
 };
 export const SAML_METADATA_URL =
   "https://login.microsoftonline.com/6c62ce1b-36c4-4f2d-a827-069c37e92080/federationmetadata/2007-06/federationmetadata.xml?appid=44302f3d-8474-4981-988d-0bc17cc1d193";
 
 export const AMPLIFY_URL =
-  "https://default-cognito.d12yp55vp6fc22.amplifyapp.com";
+  "https://default-cognito.d2n6429g3ylm0c.amplifyapp.com";
