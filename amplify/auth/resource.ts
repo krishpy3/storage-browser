b6bddf0381b6828ee345f645788a69f266f0f92e
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-06-15 01:55:28 +0530

    t8

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -27,7 +27,7 @@ export const auth = defineAuth({
           email:
             "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
           custom: {
-            groups:
+            "custom:groups":
               "http://schemas.microsoft.com/ws/2008/06/identity/claims/groups",
           },
         },
