a6f450e9416138fdfb226989f8091ce4443f02d9
Navaneethakrishnan <34976735+ItsDevMonk@users.noreply.github.com>
2025-06-15 01:23:35 +0530

    t7

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -23,7 +23,14 @@ export const auth = defineAuth({
           metadataContent: SAML_METADATA_URL, // or content of the metadata file
           metadataType: "URL",
         },
-        attributeMapping: ATTRIBUTE_MAPPING,
+        attributeMapping: {
+          email:
+            "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
+          custom: {
+            groups:
+              "http://schemas.microsoft.com/ws/2008/06/identity/claims/groups",
+          },
+        },
       },
       callbackUrls: [AMPLIFY_URL],
       logoutUrls: [AMPLIFY_URL + "/logout/"],
